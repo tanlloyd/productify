f3e61efe2db9559894b11308a6a2465d22eb2f00
tanlloyd <86486747+tanlloyd@users.noreply.github.com>
2026-01-21 01:30:01 +0800

    fix: remove extraneous period in QueryClientProvider component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,7 +22,7 @@ createRoot(document.getElementById('root')).render(   // render app inside <div 
   <StrictMode>    {/* check for safer code */}
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>  {/* enable login users everywhere in app */}
       <BrowserRouter> {/* enable to access all routes in app */}
-      <QueryClientProvider client={queryClient}>. {/* make tanstack query available everywhere in the app */}
+      <QueryClientProvider client={queryClient}> {/* make tanstack query available everywhere in the app */}
           <App />
         </QueryClientProvider>
       </BrowserRouter>
